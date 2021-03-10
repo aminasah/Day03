4a539fe80290e8945f07dcc749b85fb9f32c6d14
a_sahovic565@hotmail.com <a_sahovic565@hotmail.com>
2021-03-10 22:06:07 +0100

    First

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    // Only change code below this line
+   // make do wile loop
+ 
+ /*
+ struct student{
+     char *name;
+     char *surname;
+ };
+ */
+    //Only change code above this line
+}
+
+console.log(main());
+ 
+module.exports = main;
---
+++ ex09/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo){
+    // Only change code below this line
+      var difference= numOne - numTwo;
+    //Only change code above this line
+    
+
+    return difference;
+}
+
+console.log(main(15, 8));
+
+module.exports = main;
